ea668e73657410d3020e814b52d0f3d88897a9ee
jldeen <jedeen@outlook.com>
2018-11-12 22:20:49 -0500

    demo change

--- node-demo.js
+++ node-demo.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
     console.log('Server received request.');
-    figlet('Hello Jenkins World Nice!!!', (err, data) => {
+    figlet('Hello DevOps Days Chattanooga!!!', (err, data) => {
       if (err) {
             console.log('Something went wrong... ');
             console.dir(err);
