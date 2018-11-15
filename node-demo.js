aeb053c68dd5d2ef420de3819691fcdd8977c202
jldeen <jedeen@outlook.com>
2018-11-15 01:48:42 -0500

    demo change

--- node-demo.js
+++ node-demo.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
     console.log('Server received request.');
-    figlet('Hello DevOps Days Chattanooga!!!', (err, data) => {
+    figlet('Hello Disney!!!', (err, data) => {
       if (err) {
             console.log('Something went wrong... ');
             console.dir(err);
