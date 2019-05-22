5ec940c7b61133797abadcc7f82cc55e8efa57a0
jldeen <jedeen@outlook.com>
2019-05-22 09:21:10 +0200

    live demo

--- node-demo.js
+++ node-demo.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
     console.log('Server received request 🔥');
-    figlet('Hello Techorama Belgium!', (err, data) => {
+    figlet('Hello Techorama Belgium - live!', (err, data) => {
       if (err) {
             console.log('Something went wrong... ');
             console.dir(err);
